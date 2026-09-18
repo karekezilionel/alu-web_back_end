02fee171c7b6f7d267594699d2de3ce3add9f83c
karekezilionel <l.karekezi@alustudent.com>
2026-09-17 22:54:22 -0700

    done

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -5,7 +5,10 @@ import random
 
 
 async def wait_random(max_delay: int = 10) -> float:
-    """Wait for a random delay between 0 and max_delay seconds and return it."""
+    """Wait for a random delay between 0 and max_delay seconds.
+
+    Returns the delay.
+    """
     delay = random.uniform(0, max_delay)
     await asyncio.sleep(delay)
     return delay
