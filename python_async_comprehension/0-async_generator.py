2caf3e0f86c4a689383f86093d74f21a6036938b
karekezilionel <l.karekezi@alustudent.com>
2026-09-18 20:27:25 -0700

    done

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -2,10 +2,10 @@
 """Module that provides an async_generator coroutine."""
 import asyncio
 import random
-from typing import AsyncGenerator
+from typing import Generator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> Generator[float, None, None]:
     """Yield 10 random floats between 0 and 10, one per second.
 
     Yields:
